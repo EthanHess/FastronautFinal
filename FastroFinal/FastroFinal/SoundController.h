93201046cabc58f557a7c1026927f8c260d84d0f
EthanHess <ech1988@gmail.com>
2015-05-17 21:55:36 -0600

    work on launch view

---
+++ FastroFinal/FastroFinal/SoundController.h
@@ -0,0 +1,13 @@
+//
+//  SoundController.h
+//  FastroFinal
+//
+//  Created by Ethan Hess on 5/17/15.
+//  Copyright (c) 2015 Ethan Hess. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SoundController : NSObject
+
+@end
---
+++ FastroFinal/FastroFinal/SoundController.m
@@ -0,0 +1,13 @@
+//
+//  SoundController.m
+//  FastroFinal
+//
+//  Created by Ethan Hess on 5/17/15.
+//  Copyright (c) 2015 Ethan Hess. All rights reserved.
+//
+
+#import "SoundController.h"
+
+@implementation SoundController
+
+@end
