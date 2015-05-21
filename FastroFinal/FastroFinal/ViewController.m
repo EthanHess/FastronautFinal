b3992155aca9b59d15b27abf0d149d4b9cee0f7f
EthanHess <ech1988@gmail.com>
2015-05-21 16:07:13 -0600

    second view added

--- FastroFinal/FastroFinal/ViewController.m
+++ FastroFinal/FastroFinal/ViewController.m
@@ -53,7 +53,7 @@ int astroFall;
         astroFall = 0;
         self.startNewGame.hidden = NO;
         self.levelButton.hidden = NO;
-        self.fastronaut.image = [UIImage imageNamed:@"FastronautLanded"];
+        self.fastronaut.image = [UIImage imageNamed:@"greenFastroLanded"];
     }
     
 }
