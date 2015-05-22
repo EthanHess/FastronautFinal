3ba690017f7b4c7d0b99884d334bd5c9267eefff
EthanHess <ech1988@gmail.com>
2015-05-22 16:51:53 -0600

    level one view controller methods complete

--- FastroFinal/FastroFinal/LevelOneViewController.m
+++ FastroFinal/FastroFinal/LevelOneViewController.m
@@ -8,7 +8,9 @@
 
 #import "LevelOneViewController.h"
 
+int obstaclePosition;
 int fastroFlight;
+int score;
 
 @interface LevelOneViewController ()
 
@@ -29,24 +31,146 @@ int fastroFlight;
 
 @implementation LevelOneViewController
 
-- (IBAction)startGame:(id)sender {
-    
-    
-}
-
-
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    self.proceedButton.hidden = YES;
+    self.youDiedButton.hidden = YES;
+    score = 0;
+}
+
+
+- (IBAction)startGame:(id)sender {
+    
+    self.beginButton.hidden = YES;
+    
+    self.fastroTimer = [NSTimer scheduledTimerWithTimeInterval:0.05 target:self selector:@selector(fastroMoving) userInfo:nil repeats:YES];
+    
+    [self placeObstacle];
+    
+    self.obstacleTimer = [NSTimer scheduledTimerWithTimeInterval:0.05 target:self selector:@selector(obstacleMoving) userInfo:nil repeats:YES];
+}
+
+
+- (void)obstacleMoving {
+    
+    self.obstacleView.center = CGPointMake(self.obstacleView.center.x - 1, self.obstacleView.center.y);
+    
+    if (self.obstacleView.center.x < - 35) {
+        
+        [self placeObstacle];
+    }
+    
+    if (self.obstacleView.center.x == 30) {
+        
+        [self score];
+    }
+    
+    if (CGRectIntersectsRect(self.fastronaut.frame, self.obstacleView.frame)) {
+        
+        [self gameOver];
+    }
+    
+    if (self.fastronaut.center.y > self.view.frame.size.height - self.fastronaut.frame.size.height / 2) {
+        [self gameOver];
+    }
+    
+    if (self.fastronaut.center.y < 0 + self.fastronaut.frame.size.height / 2) {
+        [self gameOver];
+    }
+    
     
 }
+
+
+- (void)placeObstacle {
+    
+    int frame = self.view.frame.size.height;
+    
+    obstaclePosition = arc4random() %frame;
+    
+    self.obstacleView.center = CGPointMake(380, obstaclePosition);
+    
+}
+
+
+- (void)fastroMoving {
+    
+    self.fastronaut.center = CGPointMake(self.fastronaut.center.x, self.fastronaut.center.y - fastroFlight);
+    
+    fastroFlight = fastroFlight - 5;
+    
+    if (fastroFlight < -15) {
+        fastroFlight = -15;
+    }
+    
+    if (fastroFlight > 0) {
+        self.fastronaut.image = [UIImage imageNamed:@"greenFastroLanded"];
+    }
+    
+    if (fastroFlight < 0) {
+        self.fastronaut.image = [UIImage imageNamed:@"GreenFastro"];
+    }
+    
+}
+
+
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+    
+    fastroFlight = 30;
+    
+}
+
+
+- (void)gameOver {
+    
+    [self.fastroTimer invalidate];
+    [self.obstacleTimer invalidate];
+    
+    self.youDiedButton.hidden = NO;
+    self.obstacleView.hidden = YES;
+    self.fastronaut.hidden = YES;
+    
+    score = 0;
+    
+}
+
+
+- (void)score {
+    
+    score = score + 1;
+    
+    if (score > 5) {
+        [self.fastroTimer invalidate];
+        [self.obstacleTimer invalidate];
+        
+        self.proceedButton.hidden = NO;
+        self.obstacleView.hidden = YES;
+        self.fastronaut.hidden = YES;
+    }
+    
+    
+    
+}
+
 
 - (IBAction)resetGame:(id)sender {
     
+    self.beginButton.hidden = NO;
+    self.youDiedButton.hidden = YES;
+    self.fastronaut.hidden = NO;
+    self.obstacleView.hidden = NO;
+    
+    self.fastronaut.center = CGPointMake(self.view.frame.size.width / 2, self.view.frame.size.height /2);
     
 }
 
 
+- (void)playAudio {
+    
+    
+    
+}
 
 
 
