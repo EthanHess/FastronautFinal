d44607dc949fc9c7f971524b8c495e618398127c
EthanHess <ech1988@gmail.com>
2015-05-24 19:30:58 -0600

    views added to fourth level

---
+++ FastroFinal/FastroFinal/LevelFourViewController.h
@@ -0,0 +1,13 @@
+//
+//  LevelFourViewController.h
+//  FastroFinal
+//
+//  Created by Ethan Hess on 5/24/15.
+//  Copyright (c) 2015 Ethan Hess. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LevelFourViewController : UIViewController
+
+@end
---
+++ FastroFinal/FastroFinal/LevelFourViewController.m
@@ -0,0 +1,37 @@
+//
+//  LevelFourViewController.m
+//  FastroFinal
+//
+//  Created by Ethan Hess on 5/24/15.
+//  Copyright (c) 2015 Ethan Hess. All rights reserved.
+//
+
+#import "LevelFourViewController.h"
+
+@interface LevelFourViewController ()
+
+@end
+
+@implementation LevelFourViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
