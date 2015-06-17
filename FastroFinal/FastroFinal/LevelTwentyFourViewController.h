db8bfe94ccb6e24aa10a0dbeb10fc6726d044b9c
EthanHess <ech1988@gmail.com>
2015-06-17 13:40:58 -0600

    all views added to storyboard

---
+++ FastroFinal/FastroFinal/LevelTwentyFourViewController.h
@@ -0,0 +1,15 @@
+//
+//  LevelTwentyFourViewController.h
+//  FastroFinal
+//
+//  Created by Ethan Hess on 6/17/15.
+//  Copyright (c) 2015 Ethan Hess. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LevelTwentyFourViewController : UIViewController
+
+@property (nonatomic) BOOL isComplete;
+
+@end
